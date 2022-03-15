4f159597654d5839c075531d5f13d01840c3f239
emp yreum <gitlab@xn6.anonaddy.com>
2022-03-15 08:06:40 +0800

    added the bg text

--- src/program.rs
+++ src/program.rs
@@ -1,12 +1,55 @@
 use bevy::prelude::*;
 
+const FONT_PATH: &str = "font.otf";
+
 fn setup_camera(mut commands: Commands) {
-	commands.spawn_bundle(UiCameraBundle::default());
+    commands.spawn_bundle(UiCameraBundle::default());
+}
+
+fn setup_text(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load(FONT_PATH);
+
+    commands
+        .spawn_bundle(NodeBundle {
+            color: Color::NONE.into(),
+            style: Style {
+                position_type: PositionType::Absolute,
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                align_items: AlignItems::Center,
+                align_content: AlignContent::Center,
+                justify_content: JustifyContent::Center,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle {
+                style: Style {
+                    justify_content: JustifyContent::Center,
+                    align_self: AlignSelf::Center,
+                    ..Default::default()
+                },
+                text: Text::with_section(
+                    "Placeholder".to_string(),
+                    TextStyle {
+                        font,
+                        font_size: 64.0,
+                        color: Color::GRAY,
+                    },
+                    TextAlignment {
+                        vertical: VerticalAlign::Center,
+                        horizontal: HorizontalAlign::Center,
+                    },
+                ),
+                ..Default::default()
+            });
+        });
 }
 
 pub struct ProgramPlugin;
 impl Plugin for ProgramPlugin {
-	fn build(&self, app: &mut App) {
-		app.add_startup_system(setup_camera);
-	}
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup_camera)
+            .add_startup_system(setup_text);
+    }
 }
--- src/words.rs
+++ src/words.rs
@@ -1,33 +1,39 @@
+use bevy::prelude::*;
 use rand::{thread_rng, Rng};
 use std::fs::read_to_string;
-use bevy::prelude::*;
 
 pub struct Words(Vec<String>);
 impl Words {
-	pub fn new() -> Self {
-		let words = if let Ok(s) = read_to_string("assets/words.txt") {
-			s
-		} else {
-			eprintln!("assets/words.txt not found");
-			std::process::exit(1);
-		};
-		let w_vec: Vec<String> = words.split("\n").collect::<Vec<&str>>().iter().map(|s| {
-			s.to_string()
-		}).collect();
+    pub fn new() -> Self {
+        let words = if let Ok(s) = read_to_string("assets/words.txt") {
+            s
+        } else {
+            eprintln!("assets/words.txt not found");
+            std::process::exit(1);
+        };
+        let w_vec: Vec<String> = words
+            .split('\n')
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
 
-		Self(w_vec)
-	}
+        Self(w_vec)
+    }
 
-	pub fn get_new_word(&self) -> String {
-		let mut rng = thread_rng();
+    pub fn get_new_word(&self) -> String {
+        let mut rng = thread_rng();
 
-		self.0.get(rng.gen_range(0..self.0.len())).unwrap().to_string()
-	}
+        self.0
+            .get(rng.gen_range(0..self.0.len()))
+            .unwrap()
+            .to_string()
+    }
 }
 
 pub struct WordPlugin;
 impl Plugin for WordPlugin {
-	fn build(&self, app: &mut App) {
-		//
-	}
+    fn build(&self, app: &mut App) {
+        //
+    }
 }
